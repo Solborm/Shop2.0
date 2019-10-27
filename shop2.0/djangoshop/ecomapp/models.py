54ab31986031ec1e1a18f7aadbcde789ea291928
Vadim Rashkevich <zawerdom@gmail.com>
2019-10-28 01:25:33 +0300

    Upgrade

--- shop2.0/djangoshop/ecomapp/models.py
+++ shop2.0/djangoshop/ecomapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.core.urlresolvers import reverse
 from transliterate import translit
 
 class Category(models.Model):
@@ -10,6 +11,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs = {'category_slug': self.slug})
 
 
 def pre_save_category_slug(sender, instance, *args, **kwargs):
@@ -34,6 +38,11 @@ def image_folder(instance,filename):
     return "{0}/{1}".format(instance.slug,filename)
 
 
+class ProductManager(models.Manager):
+
+    def all(self, *args, **kwargs):
+        return super(ProductManager, self).get_queryset().filter(available = True)
+
 
 class Product(models.Model):
 
@@ -45,6 +54,10 @@ class Product(models.Model):
     image=models.ImageField(upload_to=image_folder)
     price=models.DecimalField(max_digits=9, decimal_places=2)
     available=models.BooleanField(default=True)
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs = {'product_slug': self.slug})
--- shop2.0/djangoshop/ecomapp/urls.py
+++ shop2.0/djangoshop/ecomapp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from ecomapp.views import base_view
+from ecomapp.views import base_view, category_view, product_view
 
 urlpatterns = [
+	url(r'^category/(?P<category_slug>[-\w]+)/$', category_view, name = 'category_detail'),
+	url(r'^product/(?P<product_slug>[-\w]+)/$', product_view, name = 'product_detail'),
 	url(r'^$', base_view, name='base'),
 ]
